306962a51ab95d1b8d003fe1bd49ff72549b680f
Gibson Ainge <gainge@gmail.com>
2018-08-31 16:24:36 -0600

    Finished creation functionality

--- api/objects/project.php
+++ api/objects/project.php
@@ -27,5 +27,31 @@ class Project {
 
 		return $stmt;
 	}
+
+	public function create() {
+		$query = "INSERT INTO " . $this->table_name . "
+			SET
+				id=:id, name=:name, submission_date=:submission_date, description=:description"
+
+		// Prepare query
+		$stmt = $this->conn->prepare($query);
+
+		// Sanitize the input I guess
+		$this->id = htmlspecialchars(strip_tags($this->id));
+		$this->name = htmlspecialchars(strip_tags($this->name));
+		$this->submission_date = htmlspecialchars(strip_tags($this->submission_date));
+		$this->description = htmlspecialchars(strip_tags($this->description));
+
+		// Bind the values
+		$stmt->bindParam(":id", $this->id);
+		$stmt->bindParam(":name", $this->name);
+		$stmt->bindParam(":submission_date", $this->submission_date);
+		$stmt->bindParam(":description", $this->description);
+
+		// Execute!
+		return $stmt.execute();	// Boolean value
+
+
+	}
 }
 ?>
